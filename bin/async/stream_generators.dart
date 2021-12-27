83684390871da7c2785cdf5cfc662d2249d475cc
geekhenno <henno.noman@gmail.com>
2021-12-27 12:05:23 +0300

    stream generator

---
+++ bin/async/stream_generators.dart
@@ -0,0 +1,14 @@
+void main() {
+  // we can use the collection functions
+  showMessages().map((event) => event.toUpperCase()).listen((event) {
+    print(event);
+  });
+}
+
+Stream<String> showMessages() async* {
+  yield 'Hi 👋🏻';
+  await Future.delayed(Duration(seconds: 1));
+  yield 'Iam Almuhanand 🥸';
+  await Future.delayed(Duration(seconds: 1));
+  yield 'I hope you enjoyed with this course 🥺';
+}
