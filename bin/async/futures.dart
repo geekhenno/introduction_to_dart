2afcb76224025534a7f5af57a9fdff75b7332da3
geekhenno <henno.noman@gmail.com>
2021-12-27 11:42:57 +0300

    futures

---
+++ bin/async/futures.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+Future<void> main() async {
+  const String apiUrl = 'https://jsonplaceholder.typicode.com/users';
+  print('loading ....');
+  try {
+    final response = await Dio().get(apiUrl);
+    print(response.data);
+  } catch (e) {
+    print('sorry there is an error $e');
+  }
+
+  // or we can use the other way but not recommended
+  // Dio()
+  //     .get(apiUrl)
+  //     .then((response) => print(response.data))
+  //     .catchError((e) => print('sorry there is an error $e'));
+}
